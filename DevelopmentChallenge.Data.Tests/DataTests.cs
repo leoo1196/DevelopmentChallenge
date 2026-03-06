19dca631602e97396aa07d5ecbc3d398b8369b80
Leo <larroyo@directvla.com.ar>
2026-03-06 15:50:59 -0300

    fix tests

--- DevelopmentChallenge.Data.Tests/DataTests.cs
+++ DevelopmentChallenge.Data.Tests/DataTests.cs
@@ -1,6 +1,7 @@
 ﻿using DevelopmentChallenge.Data.Classes;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DevelopmentChallenge.Data.Tests
 {
@@ -11,22 +12,22 @@ namespace DevelopmentChallenge.Data.Tests
         public void TestResumenListaVacia()
         {
             Assert.AreEqual("<h1>Lista vacía de formas!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 1));
+                FormaGeometrica.Imprimir(Enumerable.Empty<IForma>().ToList(), new LenguajeCastellano()));
         }
 
         [TestCase]
         public void TestResumenListaVaciaFormasEnIngles()
         {
             Assert.AreEqual("<h1>Empty list of shapes!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), 2));
+                FormaGeometrica.Imprimir(Enumerable.Empty<IForma>().ToList(), new LenguajeIngles()));
         }
 
         [TestCase]
         public void TestResumenListaConUnCuadrado()
         {
-            var cuadrados = new List<FormaGeometrica> {new FormaGeometrica(FormaGeometrica.Cuadrado, 5)};
+            var cuadrados = new List<IForma> {new Cuadrado(5)};
 
-            var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Castellano);
+            var resumen = FormaGeometrica.Imprimir(cuadrados, new LenguajeCastellano());
 
             Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
         }
@@ -34,14 +35,14 @@ namespace DevelopmentChallenge.Data.Tests
         [TestCase]
         public void TestResumenListaConMasCuadrados()
         {
-            var cuadrados = new List<FormaGeometrica>
+            var cuadrados = new List<IForma>
             {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 1),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 3)
+                new Cuadrado(5),
+                new Cuadrado(1),
+                new Cuadrado(3)
             };
 
-            var resumen = FormaGeometrica.Imprimir(cuadrados, FormaGeometrica.Ingles);
+            var resumen = FormaGeometrica.Imprimir(cuadrados, new LenguajeIngles());
 
             Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
         }
@@ -49,18 +50,18 @@ namespace DevelopmentChallenge.Data.Tests
         [TestCase]
         public void TestResumenListaConMasTipos()
         {
-            var formas = new List<FormaGeometrica>
+            var formas = new List<IForma>
             {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Circulo, 3),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 2),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 9),
-                new FormaGeometrica(FormaGeometrica.Circulo, 2.75m),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4.2m)
+                new Cuadrado(5),
+                new Circulo(3),
+                new TrianguloEquilatero(4),
+                new Cuadrado(2),
+                new TrianguloEquilatero(9),
+                new Circulo(2.75m),
+                new TrianguloEquilatero(4.2m)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Ingles);
+            var resumen = FormaGeometrica.Imprimir(formas, new LenguajeIngles());
 
             Assert.AreEqual(
                 "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65",
@@ -70,18 +71,18 @@ namespace DevelopmentChallenge.Data.Tests
         [TestCase]
         public void TestResumenListaConMasTiposEnCastellano()
         {
-            var formas = new List<FormaGeometrica>
+            var formas = new List<IForma>
             {
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 5),
-                new FormaGeometrica(FormaGeometrica.Circulo, 3),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4),
-                new FormaGeometrica(FormaGeometrica.Cuadrado, 2),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 9),
-                new FormaGeometrica(FormaGeometrica.Circulo, 2.75m),
-                new FormaGeometrica(FormaGeometrica.TrianguloEquilatero, 4.2m)
+                new Cuadrado(5),
+                new Circulo(3),
+                new TrianguloEquilatero(4),
+                new Cuadrado(2),
+                new TrianguloEquilatero(9),
+                new Circulo(2.75m),
+                new TrianguloEquilatero(4.2m)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, FormaGeometrica.Castellano);
+            var resumen = FormaGeometrica.Imprimir(formas, new LenguajeCastellano());
 
             Assert.AreEqual(
                 "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65",
